3f3277b3cf4c6d0ccc1a45941e4a3df7c4be4107
glab2411 <omar.mahmoud2411@gmail.com>
2025-04-14 07:04:53 +0200

    animation sliding text

---
+++ lib/features/splash/presentation/views/widget/sliding_text.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SlidingText extends StatelessWidget {
+  const SlidingText({
+    super.key,
+    required this.animation,
+  });
+
+  final Animation<Offset> animation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (BuildContext context, _) {  
+        return SlideTransition(
+        position: animation,
+        child: Text('Read Free Books', textAlign: TextAlign.center),
+      );
+    },
+    );
+  }
+}
--- lib/features/splash/presentation/views/widget/splash_view_body.dart
+++ lib/features/splash/presentation/views/widget/splash_view_body.dart
@@ -1,16 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:readly/core/utils/assets.dart';
+import 'package:readly/features/splash/presentation/views/widget/sliding_text.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> animation;
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+    animation = Tween<Offset>(
+      begin: const Offset(0, 10),
+      end: Offset.zero,
+    ).animate(animationController);
+    animation.addListener(() {
+      setState(() {});
+    });
+    animationController.forward();
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    // Dispose of the animation controller when the widget is removed from the widget tree
+    animationController.dispose();
+    
+  }
+
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [Image.asset(AssetsData.logo,width: 200,height: 50,),
-      const Text('Read Free Books',textAlign: TextAlign.center,),],
+      children: [
+        Image.asset(AssetsData.logo, width: 200, height: 50),
+        SlidingText(animation: animation),
+      ],
     );
   }
 }
+
